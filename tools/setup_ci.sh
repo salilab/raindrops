81191203ffcf9147008ddf83696ae26fc0f9708b
Ben Webb <ben@salilab.org>
2023-02-02 16:40:08 -0800

    We need STRIDE to run tests

--- tools/setup_ci.sh
+++ tools/setup_ci.sh
@@ -2,15 +2,32 @@
 
 # Set up an environment to run CI tests, e.g. with GitHub Actions or Travis
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 python_version"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 top_directory python_version"
   exit 1
 fi
 
-python_version=$1
+top_dir=$1
+python_version=$2
+bin_dir=${top_dir}/bin
+mkdir -p ${bin_dir}
 
 conda config --remove channels defaults  # get conda-forge, not main, packages
 conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} pip biopython networkx dill pandas scipy matplotlib imp-nightly eigen swig cmake
 eval "$(conda shell.bash hook)"
 conda activate python${python_version}
 pip install pytest-cov coverage flake8
+
+temp_dir=`mktemp -d`
+cd ${temp_dir}
+
+# STRIDE
+if [ ! -e ${bin_dir}/stride ]; then
+  wget http://webclu.bio.wzw.tum.de/stride/stride.tar.gz
+  tar -xvzf stride.tar.gz
+  make
+  mv stride ${bin_dir}/stride
+fi
+
+cd /
+rm -rf ${temp_dir}
